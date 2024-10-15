e279a421a161844e7484fe5df2a1e09e35c00185
Long Nguyen <barnonguyen@gmail.com>
2024-10-14 22:52:40 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import LogInPage from './logIn_Page/logIn.jsx';
-import RegisterPage from './Register_Page/RegisterPage.jsx'
+import  MainPage from "./Header/Header.jsx"
 
 
 function App() {
   return (
     <>
-      <RegisterPage/>
+      < MainPage/>      
     </>
 )}
 export default App
---
+++ src/Header/Header.jsx
@@ -0,0 +1,28 @@
+import "./Header.css"
+
+export default function MainPage(){
+    return (
+         <header>
+            <div className="redline-header">
+
+                <div className="logo-left-side">
+                </div>
+
+                <div className="header-title-container">
+                    <a href="#" className="title">
+                        Join the Salt'n Pepper club 
+                    </a>
+                    <span> & get $10 off your next visit</span>
+                </div>
+
+                <div className="logo-signIn" href="#">
+                    <a href="">
+                        <h4>Sign In/Sign Up</h4>
+                        <img src="" alt="" />
+                    </a>
+                </div>
+
+            </div>
+         </header>
+    )
+}
