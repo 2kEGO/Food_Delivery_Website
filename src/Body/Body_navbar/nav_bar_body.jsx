19b1578cf7d23b9f567af9f8042bc1e616c43bd1
Long Nguyen <barnonguyen@gmail.com>
2024-10-18 11:52:34 -0400

    update

--- src/Body/Body_navbar/nav_bar_body.jsx
+++ src/Body/Body_navbar/nav_bar_body.jsx
@@ -1,11 +1,16 @@
 import "./nav_bar_body.css";
+import foodImg from "./Body_navbar_img/food.jpg";
 
-export default function navBarBody() {
+export default function NavBarBody() {
     return (
         <div className="navbar-container">
             
             <div className="navbar-img-section">
-                <img src="" alt="" />
+                <img src={foodImg} alt="" />
+                <h1>Italian Inspired kitchen in the Heart of San Francisco</h1>
+                {/* <section>
+                    <img src={foodImg} alt="" />
+                </section> */}
             </div>
 
             <div className="button-container">
